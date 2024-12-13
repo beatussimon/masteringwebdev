00968e58db104acd3c0678d2a7e0ecbe091954e9
Beatus Simon <beatussimon49@gmail.com>
2024-12-13 14:08:43 +0300

    initial commit plus trying to use product array insted of hardcoding them

---
+++ javascript-amazon-project/javascript-amazon-project-main/scripts/amazon.js
@@ -0,0 +1,29 @@
+product =[
+    {
+        image : 'images/products/intermediate-composite-basketball.jpg',
+        prodcutName:'Intermediate Size Basketball',
+        rating:{
+            stars:'images/ratings/rating-40.png',
+            numbers:'127'
+        },
+        price:'2095'
+    },
+    {
+        image : 'images/products/athletic-cotton-socks-6-pairs.jpg',
+        prodcutName:'Black and Gray Athletic Cotton Socks - 6 Pairs',
+        rating:{
+            stars:'images/ratings/rating-45.png',
+            numbers:'87'
+        },
+        price:'1090'
+    },
+    {
+        image : 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+        prodcutName:'Adults Plain Cotton T-Shirt - 2 Pack',
+        rating:{
+            stars:'56',
+            numbers:'7990'
+        },
+        price:''
+    }
+]
