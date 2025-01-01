78d427c19821164ba6d91ad6964e74f0f505be00
Beatus Simon <beatussimon49@gmail.com>
2025-01-01 12:13:38 +0300

    create automated testys for format currency and fix the rounding isssue with fc function

---
+++ javascript-amazon-project/javascript-amazon-project-main/scripts/tests/moneyTest.js
@@ -0,0 +1,25 @@
+import { formatCurrency } from "../utils/money.js";
+
+console.log('test suite: formatCurrency')
+
+console.log('consverts cents into dollars')
+
+if(formatCurrency(2090) === '20.90'){
+    console.log('passed')
+}else{
+    console.log('failed')
+}
+
+console.log('works with 0')
+if(formatCurrency(0) === '0.00'){
+    console.log('passed')
+}else{
+    console.log('failed')
+}
+
+console.log('rounds up to the nearest cent')
+if(formatCurrency(2000.5) === '20.01'){
+    console.log('passed')
+}else{
+    console.log('failed')
+}
