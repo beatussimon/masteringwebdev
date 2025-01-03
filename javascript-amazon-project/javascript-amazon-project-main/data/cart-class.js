9d7687a519db8146513717ae4c61a581d75efd19
Beatus Simon <beatussimon49@gmail.com>
2025-01-03 15:59:42 +0300

    Make local storageKey and loadFromStorage private

--- javascript-amazon-project/javascript-amazon-project-main/data/cart-class.js
+++ javascript-amazon-project/javascript-amazon-project-main/data/cart-class.js
@@ -1,16 +1,16 @@
 
 class Cart{
-    cartItem = undefined;
-    localStorageKey =  undefined;
+    cartItems;
+    #localStorageKey;
 
     constructor(localStorageKey){
-        this.localStorageKey= localStorageKey;
-        this.loadFromStorage();
+        this.#localStorageKey= localStorageKey;
+        this.#loadFromStorage();
     }
 
 
-    loadFromStorage(){
-        this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+    #loadFromStorage(){
+        this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey));
         
         if(!this.cartItems){
         this.cartItems = [
@@ -28,7 +28,7 @@ class Cart{
         }
         }
     savetoLocalStorage(){
-        localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems))
+        localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems))
         }
         
     addToCart(productId){
