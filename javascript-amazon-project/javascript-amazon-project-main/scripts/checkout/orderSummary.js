88dfad455073502c01911ea4d59965bcc2e19477
Beatus Simon <beatussimon49@gmail.com>
2025-01-04 11:00:12 +0300

    consvert products into classes

--- javascript-amazon-project/javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-amazon-project/javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -73,7 +73,7 @@ return html;
                     ${matchingProduct.name}
                   </div>
                   <div class="product-price">
-                    $${formatCurrency(matchingProduct.priceCents)}
+                    $${matchingProduct.getPrice()}
                   </div>
                   <div class="product-quantity js-product-quantity-${matchingProduct.id}">
                     <span>
