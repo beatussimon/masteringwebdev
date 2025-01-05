3e63c74adffad3a9279dfb88fc537fdaa6a71ab1
Beatus Simon <beatussimon49@gmail.com>
2025-01-05 16:14:11 +0300

    Learn promises and convert our code to promises

--- javascript-amazon-project/javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project/javascript-amazon-project-main/data/cart.js
@@ -67,3 +67,14 @@ export function updateDeliveryOptions(productId, deliveryOptionId){
           matchingItem.deliveryOptionsId = deliveryOptionId;
           savetoLocalStorage()
 }
+
+export function loadCart(func){
+  const xhr = new XMLHttpRequest()
+
+  xhr.addEventListener('load',()=>{
+    console.log(xhr.response)
+    func()
+  })
+  xhr.open('GET', 'https://supersimplebackend.dev/cart')
+  xhr.send()
+}
--- javascript-amazon-project/javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project/javascript-amazon-project-main/scripts/checkout.js
@@ -3,9 +3,48 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 //import '../data/cart-class.js'
 //import '../data/backend-practice.js'
 import { loadProducts} from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 
 
-loadProducts(()=>{
+Promise.all([new Promise((resolve)=>{
+    loadProducts(()=>{
+        resolve("The string in the first resolve")
+    })
+}),   new Promise((resolve)=>{
+    loadCart(()=>{
+        resolve('iNSIDE THE RESOLVE FUNCTION')
+    })
+})]).then((value)=>{
+    console.log(value   )
     renderOrderSummary()
     renderPaymentSummary()
 })
+
+/*
+
+new Promise((resolve)=>{
+    loadProducts(()=>{
+        resolve('iNSIDE THE RESOLVE FUNCTION')
+    })
+}).then((value)=>{
+    console.log(value)
+    new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve()
+        })
+    })
+}).then(()=>{
+    renderOrderSummary()
+    renderPaymentSummary()
+})
+
+/*
+
+/*
+loadProducts(()=>{
+    loadCart(()=>{
+        renderOrderSummary()
+        renderPaymentSummary()
+    })
+ })
+ */
